320932bee2f625516b6d4d39e061971a71e4f78b
wylk2022 <100241513+wylk2022@users.noreply.github.com>
2022-02-22 19:23:54 -0300

    Add files via upload

---
+++ djangoAdmin/app/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from app.models import Proprietario, Veiculo,Acessorio
+
+# Register your models here.
+
+admin.site.register(Proprietario)
+admin.site.register(Veiculo)
+admin.site.register(Acessorio)
