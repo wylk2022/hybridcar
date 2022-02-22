0f144f12374f9f5b9920e271a5dc4a2f3c9ae0b2
wylk2022 <100241513+wylk2022@users.noreply.github.com>
2022-02-22 19:51:55 -0300

    Delete admin.py

--- djangoAdmin/app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from app.models import Proprietario, Veiculo,Acessorio
-
-# Register your models here.
-
-admin.site.register(Proprietario)
-admin.site.register(Veiculo)
-admin.site.register(Acessorio)
